3cdaf7a1cc060747ccd6fc2bd4a2cc24f7782633
Wembley-Fraggle <roman.holzner@students.fhwn.ch>
2014-09-26 19:53:14 +0200

    Javadoc

--- src/ch/fhnw/cpib/lexing/IToken.java
+++ src/ch/fhnw/cpib/lexing/IToken.java
@@ -1,7 +1,41 @@
 package ch.fhnw.cpib.lexing;
 
+/**
+ * <p>An IToken is the product of a lexing step. Tokens are used to
+ * structure a sequence of characters into a sequence of objects, which allows
+ * to analyse and parse these. Tokens are usually used to parse a
+ * syntax and build  a programm.</p>
+ * 
+ * <p>
+ * Within a text (or sequence of characters) a token is position within a text.
+ * Therefore a token has an exact position and can be located in a textual
+ * structure.
+ * </p>
+ * 
+ * <p>Some tokens may have attributes. E.g. an identifier needs the information
+ * of the identifier value.</p>
+ */
 public interface IToken {
+    
+    /**
+     * The starting postion of the token.
+     * @return The position within the text where the token starts.
+     */
 	IPosition getStart();
+	
+	/**
+	 * The length of the token. The size is the the number of characters
+	 * in a text which represent this token.
+	 * @return
+	 */
 	int getLength();
+	
+	/**
+	 * Additional attributes may give some more metadata about the 
+	 * token. E.g. identifier name or the litteral value of a number
+	 * can be read from this token.
+	 * 
+	 * @return the token
+	 */
 	Object getAttribute();
 }
