0326abb732766e3682a7f0599eb99839e89c5f4e
Wembley-Fraggle <roman.holzner@students.fhwn.ch>
2014-09-27 16:30:14 +0200

    Implementation of a IPosition

---
+++ src/ch/fhnw/cpib/lexing/Position.java
@@ -0,0 +1,30 @@
+package ch.fhnw.cpib.lexing;
+
+public class Position implements IPosition {
+
+    private int line;
+    private int column;
+    private int charPosition;
+    
+    public Position(int line, int column, int charPosition) {
+        this.line = line;
+        this.column = column;
+        this.charPosition = charPosition;
+    }
+    
+    @Override
+    public int getCurrentLine() {
+       return line;
+    }
+
+    @Override
+    public int getCurrentColumn() {
+        return column;
+    }
+
+    @Override
+    public int getCharacterPosition() {
+        return charPosition;
+    }
+
+}
