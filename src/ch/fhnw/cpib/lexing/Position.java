0d13064a789c880cbc04b489be61dc92c0bbb857
Wembley-Fraggle <roman.holzner@students.fhwn.ch>
2014-09-27 18:06:01 +0200

    Cloneing functionality added

--- src/ch/fhnw/cpib/lexing/Position.java
+++ src/ch/fhnw/cpib/lexing/Position.java
@@ -1,15 +1,35 @@
 package ch.fhnw.cpib.lexing;
 
-public class Position implements IPosition {
+public final class Position implements IPosition {
 
     private int line;
     private int column;
     private int charPosition;
     
+    /**
+     * Creates a new instance of a positon taking all parameters
+     * required to determine a position.
+     * 
+     * @param line the line which is pointed to.
+     * @param column the column in the given line which marks the position.
+     * @param charPosition the alternative value, the position in the character
+     * sequence.
+     */
     public Position(int line, int column, int charPosition) {
         this.line = line;
         this.column = column;
         this.charPosition = charPosition;
+    }
+    
+    /**
+     * Copy constructor of this instance. This can be used to clone an instance
+     * @param other the instance to be cloned.
+     */
+    public Position(Position other)
+    {
+        this.line = other.line;
+        this.column = other.column;
+        this.charPosition = other.charPosition;
     }
     
     @Override
@@ -25,5 +45,13 @@ public class Position implements IPosition {
     @Override
     public int getCharacterPosition() {
         return charPosition;
+    }
+    
+    /**
+     * Creates a new instance of this, having the same values that this
+     * instance also has.
+     */
+    public Position clone() {
+        return new Position(this);
     }
 }
--- src/ch/fhnw/cpib/test/PositionTest.java
+++ src/ch/fhnw/cpib/test/PositionTest.java
@@ -9,16 +9,39 @@ public class PositionTest {
 
     @Test
     public void testNegativeValuesAllowed() {
-        Position position = new Position(-1,-2,-3);
+        Position position = new Position(-1, -2, -3);
     }
-    
+
     @Test
     public void testGetters() {
-        Position p = new Position(1,2,3);
+        Position p = new Position(1, 2, 3);
         Assert.assertEquals(1, p.getCurrentLine());
         Assert.assertEquals(2, p.getCurrentColumn());
         Assert.assertEquals(3, p.getCharacterPosition());
+    }
+
+    @Test
+    public void testCloneConstructor() {
+        Position p = new Position(1, 2, 3);
+        Position clone = new Position(p);
+
+        Assert.assertNotSame(p, clone);
+        Assert.assertEquals(p.getCurrentLine(), clone.getCurrentLine());
+        Assert.assertEquals(p.getCurrentColumn(), clone.getCurrentColumn());
+        Assert.assertEquals(p.getCharacterPosition(),
+                clone.getCharacterPosition());
+    }
+
+    @Test
+    public void testClone() {
+        Position p = new Position(1, 2, 3);
+        Position clone = p.clone();
         
+        Assert.assertNotSame(p, clone);
+        Assert.assertEquals(p.getCurrentLine(), clone.getCurrentLine());
+        Assert.assertEquals(p.getCurrentColumn(), clone.getCurrentColumn());
+        Assert.assertEquals(p.getCharacterPosition(),
+                clone.getCharacterPosition());
     }
 
 }
