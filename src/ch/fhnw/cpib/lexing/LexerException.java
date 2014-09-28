d615c0535c34a3fb3625b2718882bd5bd53e1de9
Wembley-Fraggle <roman.holzner@students.fhwn.ch>
2014-09-28 14:30:40 +0200

    temporary Exception

---
+++ src/ch/fhnw/cpib/lexing/LexerException.java
@@ -0,0 +1,21 @@
+package ch.fhnw.cpib.lexing;
+
+public class LexerException extends Exception{
+
+    public LexerException() {
+        super();
+    }
+
+    public LexerException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public LexerException(String message) {
+        super(message);
+    }
+
+    public LexerException(Throwable cause) {
+        super(cause);
+    }
+
+}
