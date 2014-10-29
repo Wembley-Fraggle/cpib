38ff172cb2e96d608f3c88078fd1d50c46a2d598
Wembley-Fraggle <roman.holzner@students.fhwn.ch>
2014-10-29 21:06:19 +0100

    Removed FIXME

--- src/ch/fhnw/cpib/parsing/nodes/Terminal.java
+++ src/ch/fhnw/cpib/parsing/nodes/Terminal.java
@@ -4,7 +4,6 @@ import ch.fhnw.cpib.lexing.TokenType;
 
 public class Terminal extends AbstractProductionNode implements ITerminal {
     
-    // FIXME Remove this
     public Terminal(String name) {
         super(name);
     }
