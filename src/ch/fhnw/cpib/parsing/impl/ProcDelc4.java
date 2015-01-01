df8ea57224bb880f987d1990b8a999ebee5d10a9
Wembley-Fraggle <roman.holzner@students.fhwn.ch>
2015-01-01 22:07:34 +0100

    Concrete Node: ProcDecl4

--- src/ch/fhnw/cpib/parsing/impl/ProcDelc4.java
+++ src/ch/fhnw/cpib/parsing/impl/ProcDelc4.java
@@ -6,6 +6,7 @@ import ch.fhnw.cpib.parsing.IProcDecl4;
 public class ProcDelc4 implements IProcDecl4 {
 
     private IPostcondition postcondition;
+
     public ProcDelc4(IPostcondition postcondition) {
         this.postcondition = postcondition;
     }
