363ca158cebbb60835333f70458f1740804d9b26
Wembley-Fraggle <roman.holzner@students.fhwn.ch>
2015-01-01 23:32:14 +0100

    Concrete Node: Expr

---
+++ src/ch/fhnw/cpib/parsing/impl/Expr.java
@@ -0,0 +1,17 @@
+package ch.fhnw.cpib.parsing.impl;
+
+import ch.fhnw.cpib.parsing.IExpr;
+import ch.fhnw.cpib.parsing.IExpr1;
+import ch.fhnw.cpib.parsing.ITerm1;
+
+public class Expr implements IExpr {
+
+    private ITerm1 term1;
+    private IExpr1 expr1;
+    
+    public Expr(ITerm1 term1, IExpr1 expr1) {
+        this.term1 = term1;
+        this.expr1 = expr1;
+    }
+
+}
