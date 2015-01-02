c8527a8b8f056f7b2fea6acf06126616a59042ae
Wembley-Fraggle <roman.holzner@students.fhwn.ch>
2015-01-02 23:57:28 +0100

    DebougOUt

--- src/ch/fhnw/cpib/parsing/impl/Cmd6.java
+++ src/ch/fhnw/cpib/parsing/impl/Cmd6.java
@@ -3,6 +3,8 @@ package ch.fhnw.cpib.parsing.impl;
 import ch.fhnw.cpib.lexing.IToken;
 import ch.fhnw.cpib.parsing.ICmd6;
 import ch.fhnw.cpib.parsing.IExpr;
+import ch.fhnw.cpib.parsing.as.IAbsDebOutCmd;
+import ch.fhnw.cpib.parsing.as.impl.AbsDebOutCmd;
 
 public class Cmd6 implements ICmd6 {
     IToken debugOut;
@@ -11,5 +13,9 @@ public class Cmd6 implements ICmd6 {
     public Cmd6(IToken debugOut, IExpr expr) {
         this.debugOut = debugOut;
         this.expr = expr;
+    }
+    
+    public IAbsDebOutCmd toAbsSyn() {
+        return new AbsDebOutCmd(expr.toAbsSyn());
     }
 }
