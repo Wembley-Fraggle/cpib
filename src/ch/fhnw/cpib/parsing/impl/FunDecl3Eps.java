dfc75eb61a384c3469c4bb6fcc66b23298ad4163
Stolz77 <stolz77@bluewin.ch>
2015-01-02 21:58:59 +0100

    pre/postcondition

--- src/ch/fhnw/cpib/parsing/impl/FunDecl3Eps.java
+++ src/ch/fhnw/cpib/parsing/impl/FunDecl3Eps.java
@@ -2,13 +2,14 @@ package ch.fhnw.cpib.parsing.impl;
 
 import ch.fhnw.cpib.parsing.IFunDecl3;
 import ch.fhnw.cpib.parsing.as.IAbsPrecondition;
+import ch.fhnw.cpib.parsing.as.impl.AbsPrecondition;
 
 public class FunDecl3Eps implements IFunDecl3 {
 
 	@Override
 	public IAbsPrecondition toAbsSyn() {
 		// TODO Auto-generated method stub
-		return null;
+		return new AbsPrecondition();
 	}
 
 }
