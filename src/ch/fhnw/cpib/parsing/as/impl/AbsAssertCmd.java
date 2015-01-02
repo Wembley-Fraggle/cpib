6ced3d223f33cb6d544f35cabc4392573503fb4f
Wembley-Fraggle <roman.holzner@students.fhwn.ch>
2015-01-02 23:59:52 +0100

    AssertCmd

---
+++ src/ch/fhnw/cpib/parsing/as/impl/AbsAssertCmd.java
@@ -0,0 +1,13 @@
+package ch.fhnw.cpib.parsing.as.impl;
+
+import ch.fhnw.cpib.parsing.as.IAbsAssertCmd;
+import ch.fhnw.cpib.parsing.as.IAbsExpr;
+
+public class AbsAssertCmd implements IAbsAssertCmd {
+
+    private IAbsExpr expr;
+    public AbsAssertCmd(IAbsExpr expr) {
+        this.expr = expr;
+    }
+
+}
--- src/ch/fhnw/cpib/parsing/impl/Cmd7.java
+++ src/ch/fhnw/cpib/parsing/impl/Cmd7.java
@@ -3,6 +3,8 @@ package ch.fhnw.cpib.parsing.impl;
 import ch.fhnw.cpib.lexing.IToken;
 import ch.fhnw.cpib.parsing.ICmd7;
 import ch.fhnw.cpib.parsing.IExpr;
+import ch.fhnw.cpib.parsing.as.IAbsAssertCmd;
+import ch.fhnw.cpib.parsing.as.impl.AbsAssertCmd;
 
 public class Cmd7 implements ICmd7{
     IToken assertToken;
@@ -10,5 +12,9 @@ public class Cmd7 implements ICmd7{
     public Cmd7(IToken assertToken, IExpr expr) {
         this.assertToken = assertToken;
         this.expr = expr;
+    }
+    
+    public IAbsAssertCmd toAbsSyn() {
+        return new AbsAssertCmd(expr.toAbsSyn());
     }
 }
