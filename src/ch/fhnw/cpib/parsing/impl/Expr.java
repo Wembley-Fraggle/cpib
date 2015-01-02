56e32181551619c19763d815dc0d7f861f2e425b
Wembley-Fraggle <roman.holzner@students.fhwn.ch>
2015-01-02 20:24:05 +0100

    Abstract Syntax: Cmd1

--- src/ch/fhnw/cpib/parsing/impl/Expr.java
+++ src/ch/fhnw/cpib/parsing/impl/Expr.java
@@ -3,6 +3,7 @@ package ch.fhnw.cpib.parsing.impl;
 import ch.fhnw.cpib.parsing.IExpr;
 import ch.fhnw.cpib.parsing.IExpr1;
 import ch.fhnw.cpib.parsing.ITerm1;
+import ch.fhnw.cpib.parsing.as.IAbsExpr;
 
 public class Expr implements IExpr {
 
@@ -12,6 +13,12 @@ public class Expr implements IExpr {
     public Expr(ITerm1 term1, IExpr1 expr1) {
         this.term1 = term1;
         this.expr1 = expr1;
+    }
+
+    @Override
+    public IAbsExpr toAbsSyn() {
+        // TODO Auto-generated method stub
+        return null;
     }
 
 }
