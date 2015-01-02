2b630b0f27059328cf44e664c4dd4b35d0091bf1
Wembley-Fraggle <roman.holzner@students.fhwn.ch>
2015-01-02 20:19:55 +0100

    Abstract Syntax: Bool Oprs

--- src/ch/fhnw/cpib/parsing/impl/BooloprAnd.java
+++ src/ch/fhnw/cpib/parsing/impl/BooloprAnd.java
@@ -2,6 +2,7 @@ package ch.fhnw.cpib.parsing.impl;
 
 import ch.fhnw.cpib.lexing.IToken;
 import ch.fhnw.cpib.parsing.IBoolopr;
+import ch.fhnw.cpib.parsing.as.IAbsExpr2;
 
 public class BooloprAnd implements IBoolopr {
 
@@ -9,5 +10,9 @@ public class BooloprAnd implements IBoolopr {
 
     public BooloprAnd(IToken boolAnd) {
         this.boolAnd = boolAnd;
+    }
+    
+    public IAbsExpr2 toAbsSyn(IAbsExpr2 e) {
+        return e;
     }
 }
--- src/ch/fhnw/cpib/parsing/impl/BooloprCor.java
+++ src/ch/fhnw/cpib/parsing/impl/BooloprCor.java
@@ -2,12 +2,17 @@ package ch.fhnw.cpib.parsing.impl;
 
 import ch.fhnw.cpib.lexing.IToken;
 import ch.fhnw.cpib.parsing.IBoolopr;
+import ch.fhnw.cpib.parsing.as.IAbsExpr2;
 
 public class BooloprCor implements IBoolopr {
 
     private IToken boolOprCor;
     public BooloprCor(IToken boolOprCor) {
        this.boolOprCor = boolOprCor;
+    }
+    
+    public IAbsExpr2 toAbsSyn(IAbsExpr2 e) {
+        return e;
     }
 
 }
