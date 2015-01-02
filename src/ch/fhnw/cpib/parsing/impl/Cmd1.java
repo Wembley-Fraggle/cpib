df9f97a77697896243b9c39c1e143a910719e018
Wembley-Fraggle <roman.holzner@students.fhwn.ch>
2015-01-02 20:22:52 +0100

    Abstract Syntax: Cmd1

--- src/ch/fhnw/cpib/parsing/impl/Cmd1.java
+++ src/ch/fhnw/cpib/parsing/impl/Cmd1.java
@@ -3,16 +3,21 @@ package ch.fhnw.cpib.parsing.impl;
 import ch.fhnw.cpib.lexing.IToken;
 import ch.fhnw.cpib.parsing.ICmd1;
 import ch.fhnw.cpib.parsing.IExpr;
+import ch.fhnw.cpib.parsing.as.IAbsAssiCmd;
+import ch.fhnw.cpib.parsing.as.impl.AbsAssiCmd;
 
 public class Cmd1 implements ICmd1 {
 
-    private IExpr expr;
+    private IExpr leftExpr;
     private IToken becomes;
-    private IExpr expr2;
-    public Cmd1(IExpr expr, IToken becomes, IExpr expr2) {
-        this.expr = expr;
+    private IExpr rightExpr;
+    public Cmd1(IExpr leftExpr, IToken becomes, IExpr rightExpr) {
+        this.leftExpr = leftExpr;
         this.becomes = becomes;
-        this.expr2 = expr2;
+        this.rightExpr = rightExpr;
     }
-
+    
+    public IAbsAssiCmd toAbsSyn() {
+        return new AbsAssiCmd(leftExpr.toAbsSyn(), rightExpr.toAbsSyn());
+    }
 }
