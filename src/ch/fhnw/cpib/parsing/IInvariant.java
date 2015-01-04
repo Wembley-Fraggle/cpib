76bf421675b8ce1e5b7b456b188a5642ffd19d7a
Wembley-Fraggle <roman.holzner@students.fhwn.ch>
2015-01-04 18:30:13 +0100

    Invariant must return dbc Command

--- src/ch/fhnw/cpib/parsing/IInvariant.java
+++ src/ch/fhnw/cpib/parsing/IInvariant.java
@@ -1,12 +1,11 @@
 package ch.fhnw.cpib.parsing;
 
-import ch.fhnw.cpib.parsing.as.IAbsInvariant;
+import ch.fhnw.cpib.parsing.abs.IAbstSyn.IDbcCmd;
 
-//TODO Auto-generated method stub
 public interface IInvariant extends IConcSynNode{
 
-    IAbsInvariant toAbsSyn();
+    IDbcCmd toAbsSyn();
 
-		String toString(String indent);
+	String toString(String indent);
 
 }
