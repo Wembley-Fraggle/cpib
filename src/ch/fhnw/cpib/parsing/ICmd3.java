6ef0d4ce2f36114e597921a6dc5e0d4a57fb8f32
Wembley-Fraggle <roman.holzner@students.fhwn.ch>
2015-01-04 18:29:25 +0100

    Method is inherited

--- src/ch/fhnw/cpib/parsing/ICmd3.java
+++ src/ch/fhnw/cpib/parsing/ICmd3.java
@@ -1,9 +1,5 @@
 package ch.fhnw.cpib.parsing;
 
-import ch.fhnw.cpib.parsing.as.IAbsCmd;
-import ch.fhnw.cpib.parsing.as.IAbsWhileCmd;
 
-//TODO Auto-generated stub
 public interface ICmd3 extends IConcSynNode,ICCmd {
-    public IAbsCmd toAbsSyn();
 }
