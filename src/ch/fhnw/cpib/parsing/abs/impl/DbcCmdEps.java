5a9095ae4a8228d97bd1575796d82b60b30aa48a
Wembley-Fraggle <roman.holzner@students.fhwn.ch>
2015-01-04 18:09:42 +0100

    Dummy DBC Command because it can be optional

---
+++ src/ch/fhnw/cpib/parsing/abs/impl/DbcCmdEps.java
@@ -0,0 +1,29 @@
+package ch.fhnw.cpib.parsing.abs.impl;
+
+import ch.fhnw.cpib.parsing.abs.IAbstSyn.IDbcCmd;
+import ch.fhnw.lederer.virtualmachine.IVirtualMachine.CodeTooSmallError;
+
+public class DbcCmdEps implements IDbcCmd {
+
+    @Override
+    public void check(boolean canInit) throws ContextError {
+    }
+
+    @Override
+    public int code(int loc) throws CodeTooSmallError {
+        return loc;
+    }
+
+    @Override
+    public String toString(String indent) {
+        return indent
+                + "<DbcCmdEps>\n"
+                + indent
+                + "</DbcCmdEps>\n";
+    }
+
+    @Override
+    public int getLine() {
+        return -1;
+    }
+}
