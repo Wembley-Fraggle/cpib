653fdd13b6784cf8a9c5148d32a922b8ef2412cc
Wembley-Fraggle <roman.holzner@students.fhwn.ch>
2015-01-04 11:00:01 +0100

    Dyadic must be a Expr

--- src/ch/fhnw/cpib/parsing/as/impl/AbsDyad.java
+++ src/ch/fhnw/cpib/parsing/as/impl/AbsDyad.java
@@ -2,16 +2,18 @@ package ch.fhnw.cpib.parsing.as.impl;
 
 import ch.fhnw.cpib.lexing.ITerminal;
 import ch.fhnw.cpib.lexing.IToken;
+import ch.fhnw.cpib.parsing.IExpr;
 import ch.fhnw.cpib.parsing.as.IAbsDyad;
+import ch.fhnw.cpib.parsing.as.IAbsExpr;
 import ch.fhnw.cpib.parsing.as.IAbsSynNode;
 
 
 public class AbsDyad implements IAbsDyad{
     
     private IToken top;
-    private IAbsSynNode left;
-    private IAbsSynNode right;
-    public AbsDyad(IToken top, IAbsSynNode left,IAbsSynNode right) {
+    private IAbsExpr left;
+    private IAbsExpr right;
+    public AbsDyad(IToken top, IAbsExpr left,IAbsExpr right) {
         this.top = top;
         this.left = left;
         this.right = right;
