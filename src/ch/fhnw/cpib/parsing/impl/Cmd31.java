0a517b7f54a184a249c55e7a58126f43cefa6556
Wembley-Fraggle <roman.holzner@students.fhwn.ch>
2015-01-04 18:31:51 +0100

    Return the abstract invariant

--- src/ch/fhnw/cpib/parsing/impl/Cmd31.java
+++ src/ch/fhnw/cpib/parsing/impl/Cmd31.java
@@ -2,7 +2,7 @@ package ch.fhnw.cpib.parsing.impl;
 
 import ch.fhnw.cpib.parsing.ICmd31;
 import ch.fhnw.cpib.parsing.IInvariant;
-import ch.fhnw.cpib.parsing.as.IAbsInvariant;
+import ch.fhnw.cpib.parsing.abs.IAbstSyn.IDbcCmd;
 
 public class Cmd31 implements ICmd31 {
 
@@ -13,14 +13,14 @@ public class Cmd31 implements ICmd31 {
 	}
 
 	@Override
-	public IAbsInvariant toAbsSyn() {
-		return invariant.toAbsSyn();
-	}
-
-	@Override
 	public String toString(String indent) {
 		return indent + "<invariant>\n" + invariant.toString(indent + '\t')
 				+ indent + "</invariant>\n";
 	}
+
+    @Override
+    public IDbcCmd toAbsSyn() {
+        return invariant.toAbsSyn();
+    }
 
 }
--- src/ch/fhnw/cpib/parsing/impl/Cmd31Eps.java
+++ src/ch/fhnw/cpib/parsing/impl/Cmd31Eps.java
@@ -1,19 +1,21 @@
 package ch.fhnw.cpib.parsing.impl;
 
 import ch.fhnw.cpib.parsing.ICmd31;
+import ch.fhnw.cpib.parsing.abs.IAbstSyn.IDbcCmd;
+import ch.fhnw.cpib.parsing.abs.impl.DbcCmdEps;
 import ch.fhnw.cpib.parsing.as.IAbsInvariant;
 import ch.fhnw.cpib.parsing.as.impl.AbsInvariant;
 
 public class Cmd31Eps implements ICmd31 {
-
+    
     @Override
-    public IAbsInvariant toAbsSyn() {
-       return new AbsInvariant();
+    public String toString(String indent) {
+        return indent + "<invariant>TRUE</invariant>\n";
     }
 
-		@Override
-		public String toString(String indent) {
-			return indent + "<invariant>TRUE</invariant>\n";
-		}
+    @Override
+    public IDbcCmd toAbsSyn() {
+        return new DbcCmdEps();
+    }
 
 }
