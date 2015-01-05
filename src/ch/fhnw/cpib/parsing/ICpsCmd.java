6b736520a7da11764a15c851bd2192afe2bb03dd
Wembley-Fraggle <roman.holzner@students.fhwn.ch>
2015-01-05 17:06:26 +0100

    Formatting

--- src/ch/fhnw/cpib/parsing/ICpsCmd.java
+++ src/ch/fhnw/cpib/parsing/ICpsCmd.java
@@ -2,11 +2,9 @@ package ch.fhnw.cpib.parsing;
 
 import ch.fhnw.cpib.parsing.abs.IAbstSyn;
 
-
-//TODO Auto-generated stub
 public interface ICpsCmd extends IConcSynNode {
 
     IAbstSyn.ICmd toAbsSyn();
 
-		String toString(String indent);
+    String toString(String indent);
 }
